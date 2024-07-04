c8bf1a74c72a174d4de9248316446744d58573e3
hwinareun <hwinareun@gmail.com>
2024-07-04 12:31:46 +0900

    하나 이상의 체크박스 선택 시 사이드 바에 상세 정보 나타남

--- src/component/DemoPage.tsx
+++ src/component/DemoPage.tsx
@@ -10,20 +10,67 @@ import Sidebar from "./Sidebar/Sidebar";
 const DemoPage = () => {
   const [selectedItems, setSelectedItems] = useState([]);
 
-  const handleCheckboxChange = () => {
-    /* 체크 박스 메서드 */
+  const handleCheckboxChange = (id, item) => {
+    const foundItem = selectedItems.find((i) => i.id === id);
+
+    if (foundItem) {
+      setSelectedItems(selectedItems.filter((i) => i.id !== id));
+    } else {
+      setSelectedItems([...selectedItems, item]);
+    }
   };
 
   return (
     <div>
       <div>
-          { /* 체크박스 */}
-          <label>
-            <input
-              type="checkbox"
-              onChange={() => handleCheckboxChange()}
-            />
-          </label>
+        {/* 체크박스 */}
+        <label>
+          <input
+            type="checkbox"
+            onChange={() =>
+              handleCheckboxChange(1, {
+                id: 1,
+                brand: "하림",
+                name: "닭가슴살1",
+                kcal: 100,
+                protein: 10,
+              })
+            }
+          />
+          1111111
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            onChange={() =>
+              handleCheckboxChange(2, {
+                id: 2,
+                brand: "허닭",
+                name: "닭가슴살2",
+                kcal: 200,
+                protein: 20,
+              })
+            }
+          />
+          2222222
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            onChange={() =>
+              handleCheckboxChange(3, {
+                id: 3,
+                brand: "에잇템",
+                name: "닭가슴살33",
+                kcal: 300,
+                protein: 30,
+              })
+            }
+          />
+          333333
+        </label>
       </div>
       {
         /* 선택된 아이템이 하나 이상이어야 사이드바 열림. */
