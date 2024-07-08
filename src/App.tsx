2e63ed7b48a5bb1953ac3cb47081b2007242ff85
hwinareun <hwinareun@gmail.com>
2024-07-08 21:14:52 +0900

    rename: 컴포넌트 폴더 통합

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 import ChickenTable from './components/ChickenTable/ChickenTable';
 import ProductsFilter from './components/Products/ProductsFilter';
 import { ChickenData } from './types/ChickenData';
-import DemoPage from './component/DemoPage'
+import DemoPage from "./components/DemoPage";
+import Sidebar from "./components/Sidebar/Sidebar";
 import './App.css';
 
 function App() {
