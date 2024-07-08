17c712985839603b9904fc17cb47538b08213b22
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-07-08 09:04:51 +0900

    feat: Redux를 사용한 상태 관리 통합 #15
    - react-redux 패키지 추가
    - store, chicken slice 생성
    - ChickenTable 컴포넌트가 Redux를 사용하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
