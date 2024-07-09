39a005aab0f5256b60e77784f3783f883a9b1b14
hwinareun <hwinareun@gmail.com>
2024-07-09 12:13:05 +0900

    rename: detail -> comparison

--- src/components/Comparison/ComparisonTable.tsx
+++ src/components/Comparison/ComparisonTable.tsx
@@ -1,100 +1,75 @@
-import { FiMinusSquare, FiXSquare } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 import {
-    button,
-    clearButton,
-    closeButton,
-    header,
-    tableBody,
-    tableHeader,
-    tableRow,
-    tableWrapper,
-    title,
-    wrapper,
+  closeButton, header, tableBody,
+  tableHeader, tableRow, tableWrapper,
+  title, wrapper,
 } from "./ComparisonTable.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { setComparisonActive, clearComparisonData } from "../../store/slices/comparisonSlice";
+import { setComparisonActive } from "../../store/slices/comparisonSlice";
 
 const ComparisonTable = () => {
-    const dispatch = useDispatch();
-    const { comparisonData, comparisonActive } = useSelector(
-        (state: RootState) => state.comparison
-    );
+  const dispatch = useDispatch();
+  const { comparisonData, comparisonActive } = useSelector(
+    (state: RootState) => state.comparison
+  );
 
-    // 선택 비교 창 닫음
-    const handleOnCloseButton = () => {
-        dispatch(setComparisonActive(false));
-    };
-    
-    // 모든 체크박스 선택 초기화
-    const handleOnClearButton = () => {
-        dispatch(clearComparisonData());
-    };
+  // 사이드바를 닫는 액션 디스패치
+  const handleOnCloseButton = () => {
+    dispatch(setComparisonActive(false));
+  };
 
-    // 사이드바가 비활성화 상태면 null을 반환하여 렌더링하지 않음
-    if (!comparisonActive) {
-        return null;
-    }
+  // 사이드바가 비활성화 상태면 null을 반환하여 렌더링하지 않음
+  if (!comparisonActive) {
+    return null;
+  }
 
-    return (
-        <div className={wrapper}>
-            <div className={header}>
-                <div className={title}>
-                    🍗 내가 가장 원하는 닭가슴살 제품은 ? 🍗
-                </div>
-                <div className={button}>
-                    <div className={closeButton} onClick={handleOnCloseButton}>
-                        <FiMinusSquare />
-                        선택 창 숨기기
-                    </div>
-                    <div className={clearButton} onClick={handleOnClearButton}>
-                        <FiXSquare />
-                        선택 전체 해제
-                    </div>
-                </div>
-            </div>
-            <table className={tableWrapper}>
-                <thead className={tableHeader}>
-                    <tr>
-                        <th> No. </th>
-                        <th> 브랜드 </th>
-                        <th> 제품명 </th>
-                        <th> 단백질(g) </th>
-                        <th> 에너지(kcal) </th>
-                        <th> 지방(g) </th>
-                        <th> 탄수화물(g) </th>
-                        <th> 당류(g) </th>
-                        <th> 나트륨(mg) </th>
-                        <th> 콜레스테롤(mg) </th>
-                        <th> 포화지방산(g) </th>
-                        <th> 중량(g) </th>
-                        <th> 제조사 </th>
-                    </tr>
-                </thead>
-                <tbody className={tableBody}>
-                    {comparisonData.map((item) => (
-                        <tr key={item.id} className={tableRow}>
-                            <td>{item.id}</td>
-                            <td>{item.brand ? item.brand : "-"}</td>
-                            <td>{item.product_name}</td>
-                            <td>{item.calories}</td>
-                            <td>{item.protein}</td>
-                            <td> {item.fat}</td>
-                            <td> {item.calbohydrate}</td>
-                            <td> {item.sugars}</td>
-                            <td> {item.sodium}</td>
-                            <td> {item.cholesterol}</td>
-                            <td> {item.saturated_fat}</td>
-                            <td> {item.weight}</td>
-                            <td>
-                                {item.manufacturer ? item.manufacturer : "-"}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className={wrapper}>
+      <div className={header}>
+        <div className={title}> 🍗 내가 가장 원하는 닭가슴살 제품은 ? 🍗 </div>
+        <FiX className={closeButton} onClick={handleOnCloseButton} />
+      </div>
+      <table className={tableWrapper}>
+        <thead className={tableHeader}>
+          <tr>
+            <th> No. </th>
+            <th> 브랜드 </th>
+            <th> 제품명 </th>
+            <th> 칼로리(kcal) </th>
+            <th> 단백질(g) </th>
+            <th> 지방(g) </th>
+            <th> 탄수화물(g) </th>
+            <th> 당류(g) </th>
+            <th> 나트륨(mg) </th>
+            <th> 콜레스테롤(mg) </th>
+            <th> 포화지방산(g) </th>
+            <th> 중량(g) </th>
+            <th> 제조사 </th>
+          </tr>
+        </thead>
+        <tbody className={tableBody}>
+          {comparisonData.map((item) => (
+            <tr key={item.id} className={tableRow}>
+              <td>{item.id}</td>
+              <td>{item.brand ? item.brand : "-"}</td>
+              <td>{item.product_name}</td>
+              <td>{item.calories}</td>
+              <td>{item.protein}</td>
+              <td> {item.fat}</td>
+              <td> {item.calbohydrate}</td>
+              <td> {item.sugars}</td>
+              <td> {item.sodium}</td>
+              <td> {item.cholesterol}</td>
+              <td> {item.saturated_fat}</td>
+              <td> {item.weight}</td>
+              <td>{item.manufacturer ? item.manufacturer : "-"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ComparisonTable;
