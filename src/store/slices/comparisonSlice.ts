d6649de56ffbc06e3809384ff48db8b6525d2361
hwinareun <hwinareun@gmail.com>
2024-07-09 19:57:47 +0900

    feat: 선택 개수 50개 초과 시 알림창 발생

--- src/store/slices/comparisonSlice.ts
+++ src/store/slices/comparisonSlice.ts
@@ -20,19 +20,28 @@ const comparisonSlice = createSlice({
         (item) => item.id === payload.id
       );
 
-      if (isComparison) {
-        state.comparisonData = state.comparisonData.filter(
-          (item) => item.id !== payload.id
-        );
-      } else {
-        state.comparisonData.push(payload);
-      }
-      state.comparisonActive = state.comparisonData.length > 0; // 사이드바 활성화 여부 업데이트
+            if (isComparison) {
+                state.comparisonData = state.comparisonData.filter(
+                    (item) => item.id !== payload.id
+                );
+            } else {
+                if (state.comparisonData.length >= 50) {
+                    alert("최대 50개까지만 선택할 수 있습니다.");
+                    return;
+                }
+                state.comparisonData.push(payload);
+            }
+            // 사이드바 활성화 여부 업데이트
+            state.comparisonActive = state.comparisonData.length > 0;
+        },
+        setComparisonActive: (state, action: PayloadAction<boolean>) => {
+            state.comparisonActive = action.payload;
+        },
+        clearComparisonData: (state) => {
+            state.comparisonData = [];
+            state.comparisonActive = false;
+        },
     },
-    setComparisonActive: (state, action: PayloadAction<boolean>) => {
-      state.comparisonActive = action.payload;
-    },
-  },
 });
 
 export const { setComparisonData, setComparisonActive } =
