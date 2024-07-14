749a41ba79f916969b3d21a7be5da1375b3da778
hwinareun <hwinareun@gmail.com>
2024-07-15 01:52:58 +0900

    feat: 최소최댓값에 따른 글자 색상 변화

--- src/store/slices/comparisonSlice.ts
+++ src/store/slices/comparisonSlice.ts
@@ -4,11 +4,63 @@ import { ChickenData } from "../../types/ChickenData";
 type ComparisonState = {
     comparisonData: ChickenData[];
     comparisonActive: boolean;
+    maxValues: {
+        protein: number | null;
+    };
+    minValues: {
+        calories: number | null;
+        fat: number | null;
+        calbohydrate: number | null;
+        sugars: number | null;
+        sodium: number | null;
+        cholesterol: number | null;
+        saturated_fat: number | null;
+    };
 };
 
 const initialState: ComparisonState = {
     comparisonData: [],
     comparisonActive: false,
+    maxValues: {
+        protein: null,
+    },
+    minValues: {
+        calories: null,
+        fat: null,
+        calbohydrate: null,
+        sugars: null,
+        sodium: null,
+        cholesterol: null,
+        saturated_fat: null,
+    },
+};
+
+const calulateMaxValues = (data: ChickenData[]) => {
+    return {
+        protein:
+            data.length > 1 ? Math.max(...data.map((i) => i.protein)) : null,
+    };
+};
+const calculateMinValues = (data: ChickenData[]) => {
+    return {
+        calories:
+            data.length > 1 ? Math.max(...data.map((i) => i.calories)) : null,
+        fat: data.length > 1 ? Math.max(...data.map((i) => i.fat)) : null,
+        calbohydrate:
+            data.length > 1
+                ? Math.max(...data.map((i) => i.calbohydrate))
+                : null,
+        sugars: data.length > 1 ? Math.max(...data.map((i) => i.sugars)) : null,
+        sodium: data.length > 1 ? Math.max(...data.map((i) => i.sodium)) : null,
+        cholesterol:
+            data.length > 1
+                ? Math.max(...data.map((i) => i.cholesterol))
+                : null,
+        saturated_fat:
+            data.length > 1
+                ? Math.max(...data.map((i) => i.saturated_fat))
+                : null,
+    };
 };
 
 const comparisonSlice = createSlice({
@@ -31,6 +83,10 @@ const comparisonSlice = createSlice({
                 }
                 state.comparisonData.push(payload);
             }
+
+            state.maxValues = calulateMaxValues(state.comparisonData);
+            state.minValues = calculateMinValues(state.comparisonData);
+
             // 사이드바 활성화 여부 업데이트
             state.comparisonActive = state.comparisonData.length > 0;
         },
