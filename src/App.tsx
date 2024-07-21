0e6f9fc9aaa3efa701dff5f24fdce4c420372035
hwinareun <hwinareun@gmail.com>
2024-07-22 05:14:18 +0900

    refactor: 스크린 크기에 따라 창 변화

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ const App: React.FC = () => {
   const [filteredData, setFilteredData] = useState<ChickenData[]>([]);
 
     return (
-        <div className="container p-4 mx-auto">
-            <div className="flex flex-col items-center justify-center p-20 lg:flex-row">
+        <div className="container p-20 mx-auto">
+            <div className="flex items-center justify-center p-20 sm:flex-col">
                 <div>
                     <ProductsFilter onFilter={setFilteredData} />
                     <SearchBox onFilter={setFilteredData} />
                     <ChickenTable filteredData={filteredData} />
                 </div>
-                <div className="{/* 화면이 특정픽셀 이하면 기존 크기보다 50% 작아져서 나타나야 함*/}">
+                <div className="w-screen">
                     <ComparisonTable />
                 </div>
             </div>
--- src/components/Comparison/ComparisonTable.tsx
+++ src/components/Comparison/ComparisonTable.tsx
@@ -20,21 +20,21 @@ const ComparisonTable = () => {
   }
 
     return (
-        <div className="flex flex-col m-auto rounded-3xl bg-chickenMain">
-            <div className="flex items-center justify-between w-full px-24 py-10 font-bold border-b-2 border-b-chickenPoint">
-                <div className="text-3xl text-chickenPoint">
-                    선택 상품 상세 정보 비교 창
+        <div className="flex flex-col rounded-3xl bg-chickenMain">
+            <div className="flex items-center justify-between w-full px-24 py-10 text-2xl font-bold border-b-2 border-b-chickenPoint sm:text-sm sm:py-0 sm:px-10">
+                <div className="text-chickenPoint">
+                    선택 상품 상세 정보 비교 창 🐔
                 </div>
-                <div className="flex text-xl opacity-70">
+                <div className="flex opacity-70">
                     <div
-                        className="flex items-center p-3 m-2 cursor-pointer rounded-3xl bg-chickenNeutral hover:bg-chickenPositive"
+                        className="flex items-center p-3 m-2 cursor-pointer rounded-3xl hover:bg-chickenPositive"
                         onClick={handleOnCloseButton}
                     >
                         <FiMinusSquare />
                         선택 창 숨기기
                     </div>
                     <div
-                        className="flex items-center p-3 m-2 cursor-pointer rounded-3xl bg-chickenNeutral hover:bg-chickenNegative"
+                        className="flex items-center p-3 m-2 cursor-pointer rounded-3xl hover:bg-chickenNegative"
                         onClick={handleOnClearButton}
                     >
                         <FiXSquare />
@@ -42,140 +42,145 @@ const ComparisonTable = () => {
                     </div>
                 </div>
             </div>
-            <table className="m-10 text-center border-collapse bg-neutral-50 whitespace-nowrap">
-                <thead className="text-lg bg-chickenPoint">
-                    <tr>
-                        <th className="p-4"> No. </th>
-                        <th className="px-12"> 브랜드 </th>
-                        <th className="px-40"> 제품명 </th>
-                        <th className="px-6"> 단백질(g) </th>
-                        <th className="px-4"> 에너지(kcal) </th>
-                        <th className="px-4"> 지방(g) </th>
-                        <th className="px-4"> 탄수화물(g) </th>
-                        <th className="px-4"> 당류(g) </th>
-                        <th className="px-4"> 나트륨(mg) </th>
-                        <th className="px-4"> 콜레스테롤(mg) </th>
-                        <th className="px-4"> 포화지방산(g) </th>
-                        <th className="px-4"> 중량(g) </th>
-                        <th className="px-20"> 제조사 </th>
-                    </tr>
-                </thead>
-                <tbody className="text-sm text-slate-600">
-                    {comparisonData.map((item) => (
-                        <tr
-                            key={item.id}
-                            className="hover:bg-chickenNeutral hover:text-black "
-                        >
-                            <td className="p-2">{item.id}</td>
-                            <td className="p-2">
-                                {item.brand ? item.brand : "-"}
-                            </td>
-                            <td className="p-2">{item.product_name}</td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.protein === maxValues.protein
-                                            ? "red"
-                                            : "inherit",
-                                }}
-                            >
-                                {item.protein}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.calories === minValues.calories
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {item.calories}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.fat === minValues.fat
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.fat}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.calbohydrate ===
-                                        minValues.calbohydrate
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.calbohydrate}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.sugars === minValues.sugars
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.sugars}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.sodium === minValues.sodium
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.sodium}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.cholesterol ===
-                                        minValues.cholesterol
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.cholesterol}
-                            </td>
-                            <td
-                                className="p-2"
-                                style={{
-                                    color:
-                                        item.saturated_fat ===
-                                        minValues.saturated_fat
-                                            ? "blue"
-                                            : "inherit",
-                                }}
-                            >
-                                {" "}
-                                {item.saturated_fat}
-                            </td>
-                            <td className="p-2"> {item.weight}</td>
-                            <td className="p-2">
-                                {item.manufacturer ? item.manufacturer : "-"}
-                            </td>
+            <div>
+                <table className="m-10 text-center bg-white border-collapse whitespace-nowrap sm:m-5">
+                    <thead className="text-base bg-chickenPoint sm:hidden">
+                        <tr>
+                            <th className="p-2"> No. </th>
+                            <th className="px-2"> 브랜드 </th>
+                            <th className="px-2"> 제품명 </th>
+                            <th className="px-2"> 단백질(g) </th>
+                            <th className="px-2"> 에너지(kcal) </th>
+                            <th className="px-2"> 지방(g) </th>
+                            <th className="px-2"> 탄수화물(g) </th>
+                            <th className="px-2"> 당류(g) </th>
+                            <th className="px-2"> 나트륨(mg) </th>
+                            <th className="px-2"> 콜레스테롤(mg) </th>
+                            <th className="px-2"> 포화지방산(g) </th>
+                            <th className="px-2"> 중량(g) </th>
+                            <th className="px-2"> 제조사 </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="text-sm text-slate-600 sm:text-xs">
+                        {comparisonData.map((item) => (
+                            <tr
+                                key={item.id}
+                                className="border-b hover:bg-chickenHover hover:text-black border-slate-200"
+                            >
+                                <td className="p-2">{item.id}</td>
+                                <td className="p-2">
+                                    {item.brand ? item.brand : "-"}
+                                </td>
+                                <td className="p-2">{item.product_name}</td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.protein === maxValues.protein
+                                                ? "red"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.protein}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.calories === minValues.calories
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.calories}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.fat === minValues.fat
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.fat}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.calbohydrate ===
+                                            minValues.calbohydrate
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.calbohydrate}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.sugars === minValues.sugars
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.sugars}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.sodium === minValues.sodium
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.sodium}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.cholesterol ===
+                                            minValues.cholesterol
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.cholesterol}
+                                </td>
+                                <td
+                                    className="p-2"
+                                    style={{
+                                        color:
+                                            item.saturated_fat ===
+                                            minValues.saturated_fat
+                                                ? "blue"
+                                                : "inherit",
+                                    }}
+                                >
+                                    {item.saturated_fat}
+                                </td>
+                                <td className="p-2"> {item.weight}</td>
+                                <td className="p-2">
+                                    {item.manufacturer
+                                        ? item.manufacturer
+                                        : "-"}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="pb-3 mr-8 text-xs text-right text-slate-600 lg:hidden">
+                (번호, 브랜드, 제품명, 단백질, 에너지, 지방, 탄수화물, 당류,
+                나트륨, 콜레스테롤, 포화지방산, 중량, 제조사 순)
+            </div>
+            <div className="pb-3 mr-8 text-sm text-right text-slate-600 sm:hidden">
+                (모든 영양성분은 100g당 기준으로 작성됨.)
+            </div>
         </div>
     );
 };
