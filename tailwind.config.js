2d973173089d15a263609fea36ad73af32de9f27
hwinareun <hwinareun@gmail.com>
2024-07-22 01:38:25 +0900

    refactor: tailwindCSS 수정 + 주요색상 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                chickenMain: " #ffe5c9",
+                chickenPoint: "#fc9d38",
+                chickenNeutral: "#fed7aa",
+                chickenPositive: "#6cbd7c",
+                chickenNegative: "#fc3838",
+            },
+        },
+    },
+    plugins: [],
+};
 
