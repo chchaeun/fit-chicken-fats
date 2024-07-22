a66353c5bf9f287e2e4171df3b3960541eb496e0
hwinareun <hwinareun@gmail.com>
2024-07-23 00:16:13 +0900

    feat: 헤더 로고 적용 && 푸터 깃허브 링크 이동 버튼 생성

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,13 +1,23 @@
-
+import { FaGithubSquare } from "react-icons/fa"
 
 const Footer = () => {
-  return (
-      <div className="border-t-2 border-t-chickenPoint">
+    const pageGithub = () => {
+        window.open("https://github.com/chchaeun/fit-chicken-fats");
+    }
+    
+    return (
+        <div className="flex justify-between border-t-2 border-t-chickenPoint">
+            <FaGithubSquare
+                className="m-3 text-4xl text-chickenMain hover:text-chickenPoint"
+                onClick={pageGithub}
+            />
             <div className="p-10 text-xs text-right text-slate-600">
-                ⓒ 2024. Team-ChickenLabs. All rights reserved. 
+                ⓒ 2024. Team-ChickenLabs. All rights reserved.
+                <br />
+                (출처: 식품의약품안전처 식품영양성분 데이터베이스)
             </div>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Footer
