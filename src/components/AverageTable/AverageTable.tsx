9605dc1e05d5ccbb91b1ecdcd1c9a1182a55a577
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-07-22 11:24:45 +0900

    feat: 중복된 필터 제거하고 전역 상태(selectedBrands) 사용 #26

--- src/components/AverageTable/AverageTable.tsx
+++ src/components/AverageTable/AverageTable.tsx
@@ -1,42 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import overallAveragesData from "../../../public/data/overall_averages.json";
 import brandAveragesData from "../../../public/data/brand_averages.json";
 import { NutritionData, BrandAverages } from "../../types/AverageData";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
 
 const overallAverages: NutritionData = overallAveragesData as NutritionData;
 const brandAverages: BrandAverages = brandAveragesData as BrandAverages;
 
 const AverageTable: React.FC = () => {
-    const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
-
-    const brands = Object.keys(brandAverages);
+    const selectedBrands = useSelector(
+        (state: RootState) => state.chicken.selectedBrands
+    )
 
     return (
         <div className="container mx-auto">
             <h2 className="text-2xl font-bold mb-4">평균 영양성분</h2>
-            <div className="flex flex-wrap mb-4">
-                {brands.map((brand) => (
-                    <label key={brand} className="mr-4 mb-2 flex items-center">
-                        <input
-                            type="checkbox"
-                            value={brand}
-                            checked={selectedBrands.includes(brand)}
-                            onChange={(event) => {
-                                const value = event.target.value;
-                                setSelectedBrands((prevSelected) =>
-                                    prevSelected.includes(value)
-                                        ? prevSelected.filter(
-                                              (b) => b !== value
-                                          )
-                                        : [...prevSelected, value]
-                                );
-                            }}
-                            className="mr-2"
-                        />
-                        {brand}
-                    </label>
-                ))}
-            </div>
             <div className="overflow-x-auto">
                 <table className="min-w-full border-collapse">
                     <thead className="bg-gray-200">
