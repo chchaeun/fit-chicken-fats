c012ea253c872dd90049bdde14d2e0ee5cf8d1ae
hwinareun <hwinareun@gmail.com>
2024-07-23 15:42:45 +0900

    refactor: 중복코드와 사용하지 않는 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import ChickenTable from "./components/ChickenTable/ChickenTable";
 import ProductsFilter from "./components/Products/ProductsFilter";
 import { ChickenData } from "./types/ChickenData";
-import "./App.css";
 import ComparisonTable from "./components/Comparison/ComparisonTable";
 import SearchBox from "./components/SearchBox/SearchBox"
 import AverageTable from "./components/AverageTable/AverageTable";
