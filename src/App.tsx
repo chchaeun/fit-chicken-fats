16b934fd1c827c6909ced0eeee5c9b108752c8c7
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-07-23 10:02:55 +0900

    refactor: 사용하지 않는 변수 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ChickenTable from "./components/ChickenTable/ChickenTable";
 import ProductsFilter from "./components/Products/ProductsFilter";
-import { ChickenData } from "./types/ChickenData";
 import "./App.css";
 import ComparisonTable from "./components/Comparison/ComparisonTable";
-import SearchBox from "./components/SearchBox/SearchBox"
+import SearchBox from "./components/SearchBox/SearchBox";
 
 const App: React.FC = () => {
-    const [filteredData, setFilteredData] = useState<ChickenData[]>([]);
-
     return (
         <div>
-            <ProductsFilter onFilter={setFilteredData} />
-            <ChickenTable filteredData={filteredData} />
-            <SearchBox onFilter={setFilteredData} />
+            <ProductsFilter />
+            <ChickenTable />
+            <SearchBox />
             <ComparisonTable />
         </div>
     );
