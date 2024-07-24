a5f7de8771d96e2ca8133125cc833790312305f4
hwinareun <hwinareun@gmail.com>
2024-07-24 14:04:48 +0900

    refactor: 헤더 padding & 툴팁 수정

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,5 +1,5 @@
 import ChickenLabsLogo from "../../assets/imges/ChickenLabsLogo.png"
-import { GrCircleQuestion } from "react-icons/gr";
+import { GoQuestion } from "react-icons/go";
 
 interface HeaderProps {
     onMouseEnter: () => void;
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ onMouseEnter, onMouseLeave }) => {
     return (
         <div className="w-full border-b-2 border-b-chickenPoint">
-            <div className="flex items-center justify-between p-4 text-3xl text-chickenMain">
+            <div className="flex items-center justify-between px-4 text-3xl text-chickenMain">
                 <div className="flex flex-col items-center">
                     <div className="flex items-center">
                         <a href="/">
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ onMouseEnter, onMouseLeave }) => {
                         </a>
                     </div>
                 </div>
-                <GrCircleQuestion
+                <GoQuestion
                     className="mr-3 text-4xl rounded-full cursor-pointer text-chickenMain hover:text-white hover:bg-chickenPoint"
                     onMouseEnter={onMouseEnter}
                     onMouseLeave={onMouseLeave}
